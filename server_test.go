ad8bdb036166eb349c173e184387034dfc993e91
jason <2631937454@qq.com>
2023-04-27 14:37:39 +0800

    集成测试通配符和参数路由

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package geek_web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -14,6 +15,17 @@ func TestServer(t *testing.T) {
 	s.GET("/user/login", func(ctx *Context) {
 		ctx.Response.WriteHeader(http.StatusOK)
 		_, _ = ctx.Response.Write([]byte("/user/login"))
+	})
+	s.GET("/assets/*filepath", func(ctx *Context) {
+		filepath := ctx.Params["filepath"]
+		ctx.Response.WriteHeader(http.StatusOK)
+		_, _ = ctx.Response.Write([]byte(fmt.Sprintf("你是想找【%s】文件吗？", filepath)))
+	})
+	s.GET("/user/:id/:action", func(ctx *Context) {
+		id := ctx.Params["id"]
+		action := ctx.Params["action"]
+		ctx.Response.WriteHeader(http.StatusOK)
+		_, _ = ctx.Response.Write([]byte(fmt.Sprintf("你是不是想对ID是%s的用户进行%s操作", id, action)))
 	})
 	_ = s.Start(":8080")
 }
