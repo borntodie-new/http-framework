533f161171a85815eb2f9516ec663efe2a7908db
jason <2631937454@qq.com>
2023-04-28 09:01:06 +0800

    测试路由分组具体功能

--- server_test.go
+++ server_test.go
@@ -7,19 +7,20 @@ import (
 
 func TestServer(t *testing.T) {
 	s := NewHTTPServer()
-	s.GET("/user", func(ctx *Context) {
+	v1 := s.Group("/v1")
+	v1.GET("/user", func(ctx *Context) {
 		_ = ctx.JSON(http.StatusOK, H{
 			"code": 200,
 			"msg":  "请求成功" + ctx.Pattern,
 		})
 	})
-	s.GET("/user/login", func(ctx *Context) {
+	v1.GET("/user/login", func(ctx *Context) {
 		_ = ctx.JSON(http.StatusOK, H{
 			"code": 200,
 			"msg":  "请求成功" + ctx.Pattern,
 		})
 	})
-	s.GET("/assets/*filepath", func(ctx *Context) {
+	v1.GET("/assets/*filepath", func(ctx *Context) {
 		filePath, _ := ctx.Param("filepath")
 		_ = ctx.JSON(http.StatusOK, H{
 			"code": 200,
@@ -27,7 +28,7 @@ func TestServer(t *testing.T) {
 			"info": "你是想访问我的这个文件吗？【" + filePath + "]",
 		})
 	})
-	s.GET("/user/:id/:action", func(ctx *Context) {
+	v1.GET("/user/:id/:action", func(ctx *Context) {
 		id, _ := ctx.Param("id")
 		action, _ := ctx.Param("action")
 		_ = ctx.JSON(http.StatusOK, H{
