50eba349e81de7247fe413eb8ee543a69f1f6b6c
jason <2631937454@qq.com>
2023-04-29 15:05:14 +0800

    测试模板渲染功能

--- test/server_test.go
+++ test/server_test.go
@@ -10,7 +10,10 @@ import (
 )
 
 func TestServer(t *testing.T) {
-	s := geek_web.NewHTTPServer()
+	tplEngine := geek_web.NewGoTemplateEngine()
+	// 初始化server是加上配置
+	// 表示需要一个带有TemplateEngine的server
+	s := geek_web.NewHTTPServer(geek_web.ServerWithTemplateEngine(tplEngine))
 	// 我们注册一个全局的中间件
 	s.Use(func(next geek_web.HandleFunc) geek_web.HandleFunc {
 		return func(ctx *geek_web.Context) {
@@ -28,14 +31,14 @@ func TestServer(t *testing.T) {
 		v1.GET("/user", func(ctx *geek_web.Context) {
 			s := []int{1, 2, 3}
 			fmt.Println(s[100]) // 这里直接报错：下标越界
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"method": ctx.Method,
 			})
 		})
 		v1.GET("/user/login", func(ctx *geek_web.Context) {
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"method": ctx.Method,
@@ -43,7 +46,7 @@ func TestServer(t *testing.T) {
 		})
 		v1.GET("/assets/*filepath", func(ctx *geek_web.Context) {
 			filePath, _ := ctx.Param("filepath")
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"info":   "你是想访问我的这个文件吗？【" + filePath + "]",
@@ -53,7 +56,7 @@ func TestServer(t *testing.T) {
 		v1.GET("/user/:id/:action", func(ctx *geek_web.Context) {
 			id, _ := ctx.Param("id")
 			action, _ := ctx.Param("action")
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"id":     id,
@@ -65,14 +68,14 @@ func TestServer(t *testing.T) {
 	v2 := s.Group("/v2")
 	{
 		v2.POST("/user", func(ctx *geek_web.Context) {
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"method": ctx.Method,
 			})
 		})
 		v2.POST("/user/login", func(ctx *geek_web.Context) {
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"method": ctx.Method,
@@ -80,7 +83,7 @@ func TestServer(t *testing.T) {
 		})
 		v2.POST("/assets/*filepath", func(ctx *geek_web.Context) {
 			filePath, _ := ctx.Param("filepath")
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"info":   "你是想访问我的这个文件吗？【" + filePath + "]",
@@ -90,13 +93,30 @@ func TestServer(t *testing.T) {
 		v2.POST("/user/:id/:action", func(ctx *geek_web.Context) {
 			id, _ := ctx.Param("id")
 			action, _ := ctx.Param("action")
-			_ = ctx.JSON(http.StatusOK, geek_web.H{
+			ctx.JSON(http.StatusOK, geek_web.H{
 				"code":   200,
 				"msg":    "请求成功" + ctx.Pattern,
 				"id":     id,
 				"action": action,
 				"method": ctx.Method,
 			})
+		})
+	}
+	v3 := s.Group("/v3")
+	err := tplEngine.ParseGlob("../template/*.gohtml") // 用gohtml作为模板文件的后缀名，Goland会识别到这是一个模板文件
+	if err != nil {
+		panic("Web: 解析文件失败")
+	}
+	{
+		v3.GET("/login", func(ctx *geek_web.Context) {
+			data := struct {
+				Username string
+				Password string
+			}{
+				Username: "Neo",
+				Password: "Neo123",
+			}
+			ctx.HTML(http.StatusOK, "login.gohtml", data)
 		})
 	}
 	_ = s.Start(":8080")
