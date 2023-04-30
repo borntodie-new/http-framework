34d6004dc35c21ce0c9a8691e1d4af9410b93806
jason <2631937454@qq.com>
2023-04-30 11:31:31 +0800

    实现中间件间的通信||状态码和响应体的零值情况

--- context.go
+++ context.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 )
 
 // Context 请求响应的上下文，请求过来到响应回去的全过程
@@ -35,6 +37,19 @@ type Context struct {
 
 	// 模板引擎对象
 	t TemplateEngine
+
+	// mu 加上读写锁，保护Keys信息
+	mu sync.RWMutex
+	// Keys 是一个键值对，实现中间件之间通信
+	Keys map[string]any
+	// 上面两个属性，我们是在实现session的时候，思考session怎么存储能够在其他地方能够直接获取到而来的灵感。
+	// 其实对于Gin框架也是有这样的设计的。我们这也就刚好和Gin不谋而合。
+	// 这个功能其实是非常常见的：就是怎么实现中间件的通信
+	// 个人认为没有必要加上一个锁，这个锁的目的是为了保护Keys
+	// Keys的作用是保存某些数据供其他中间件更方便使用
+	// 中间件的执行是线性的，就是执行一个中间件，才能进入到下一个中间件
+	// 应该是不存在并发的对Keys进行读写操作
+	// 但是呢，人Gin使用了，那咱们也就加上一下吧，难度不是很大
 }
 
 func newContext(w http.ResponseWriter, r *http.Request) *Context {
@@ -44,6 +59,8 @@ func newContext(w http.ResponseWriter, r *http.Request) *Context {
 		Method:   r.Method,
 		Pattern:  r.URL.Path,
 		header:   map[string]string{},
+		status:   http.StatusOK, // 默认是200，因为状态码不能设置为0，但是int类型的零值是0
+		data:     []byte(""),    // 响应体默认设置为空字符串吧，好像说响应体也不能为零值，对于这个还有点特殊，因为我们定义data类型是any类型的，也可以直接将Context中的data改成[]byte类型
 	}
 }
 
@@ -151,6 +168,139 @@ func (c *Context) Form(key string) (string, error) {
 // SetCookie 这种方法其实没必要封装，因为http包已经内置了一个非常简单的方法
 func (c *Context) SetCookie(cookie *http.Cookie) {
 	http.SetCookie(c.Response, cookie)
+}
+
+// Set is used to store a new key/value pair exclusively for this context.
+// It also lazy initializes  c.Keys if it was not used previously.
+func (c *Context) Set(key string, value any) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.Keys == nil {
+		c.Keys = make(map[string]any)
+	}
+
+	c.Keys[key] = value
+}
+
+// Get returns the value for the given key, ie: (value, true).
+// If the value does not exist it returns (nil, false)
+func (c *Context) Get(key string) (value any, exists bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	value, exists = c.Keys[key]
+	return
+}
+
+// MustGet 特别注意下个方法，如果没有获取到信息，会直接panic的
+func (c *Context) MustGet(key string) any {
+	if value, exists := c.Get(key); exists {
+		return value
+	}
+	panic("Key \"" + key + "\" does not exist")
+}
+
+// GetString returns the value associated with the key as a string.
+func (c *Context) GetString(key string) (s string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		s, _ = val.(string)
+	}
+	return
+}
+
+// GetBool returns the value associated with the key as a boolean.
+func (c *Context) GetBool(key string) (b bool) {
+	if val, ok := c.Get(key); ok && val != nil {
+		b, _ = val.(bool)
+	}
+	return
+}
+
+// GetInt returns the value associated with the key as an integer.
+func (c *Context) GetInt(key string) (i int) {
+	if val, ok := c.Get(key); ok && val != nil {
+		i, _ = val.(int)
+	}
+	return
+}
+
+// GetInt64 returns the value associated with the key as an integer.
+func (c *Context) GetInt64(key string) (i64 int64) {
+	if val, ok := c.Get(key); ok && val != nil {
+		i64, _ = val.(int64)
+	}
+	return
+}
+
+// GetUint returns the value associated with the key as an unsigned integer.
+func (c *Context) GetUint(key string) (ui uint) {
+	if val, ok := c.Get(key); ok && val != nil {
+		ui, _ = val.(uint)
+	}
+	return
+}
+
+// GetUint64 returns the value associated with the key as an unsigned integer.
+func (c *Context) GetUint64(key string) (ui64 uint64) {
+	if val, ok := c.Get(key); ok && val != nil {
+		ui64, _ = val.(uint64)
+	}
+	return
+}
+
+// GetFloat64 returns the value associated with the key as a float64.
+func (c *Context) GetFloat64(key string) (f64 float64) {
+	if val, ok := c.Get(key); ok && val != nil {
+		f64, _ = val.(float64)
+	}
+	return
+}
+
+// GetTime returns the value associated with the key as time.
+func (c *Context) GetTime(key string) (t time.Time) {
+	if val, ok := c.Get(key); ok && val != nil {
+		t, _ = val.(time.Time)
+	}
+	return
+}
+
+// GetDuration returns the value associated with the key as a duration.
+func (c *Context) GetDuration(key string) (d time.Duration) {
+	if val, ok := c.Get(key); ok && val != nil {
+		d, _ = val.(time.Duration)
+	}
+	return
+}
+
+// GetStringSlice returns the value associated with the key as a slice of strings.
+func (c *Context) GetStringSlice(key string) (ss []string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		ss, _ = val.([]string)
+	}
+	return
+}
+
+// GetStringMap returns the value associated with the key as a map of interfaces.
+func (c *Context) GetStringMap(key string) (sm map[string]any) {
+	if val, ok := c.Get(key); ok && val != nil {
+		sm, _ = val.(map[string]any)
+	}
+	return
+}
+
+// GetStringMapString returns the value associated with the key as a map of strings.
+func (c *Context) GetStringMapString(key string) (sms map[string]string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		sms, _ = val.(map[string]string)
+	}
+	return
+}
+
+// GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
+func (c *Context) GetStringMapStringSlice(key string) (smss map[string][]string) {
+	if val, ok := c.Get(key); ok && val != nil {
+		smss, _ = val.(map[string][]string)
+	}
+	return
 }
 
 // 下述方法这只是临时性的，我们后面会通过AOP的形式完成自动设置状态码、响应头和响应数据到Response中
